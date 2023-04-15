e8dcc209a4b7d96525edac4463b119ddb031d59d
Eric Truett <etruett@alas.com>
2023-04-14 19:15:50 -0500

    Working version of scheduler page

---
+++ app/helper.py
@@ -0,0 +1,98 @@
+from collections import defaultdict
+import datetime
+import json
+import itertools
+from typing import Any, Dict, List, Tuple
+
+
+def get_timestamp() -> datetime.datetime:
+    today = datetime.datetime.now()
+    return datetime.datetime(today.year, today.month, today.day, 19, 0, 0)
+
+
+def number_to_word(n: Any) -> str:
+        return {'1': "One", '2': "Two", '3': "Three", '4': "Four", '5': "Five", '6': "Six",
+                '7': "Seven", '8': "Eight", '9': "Nine", '10': "Ten", '11': "Eleven", '12': "Twelve",
+                '13': "Thirteen", '14': "Fourteen", '15': "Fifteen", '20': "Twenty", '25': "Twenty-Five", '30': 'Thirty'}.get(str(n))
+
+
+def readable_schedule(players: list, sched: str, sep=' - '):
+    """Creates readable schedule"""
+    # data has players and schedule keys
+    if not sched:
+        return [1, 1, 'No available schedule', 'No available schedule']
+    items = []
+    if isinstance(sched, str):
+        sched = json.loads(sched)
+    for idx, rnd in enumerate(sched):
+        court = 1
+        for matchup in rnd:
+            team1 = sep.join([players[int(i)].strip() for i in matchup[0:2]])
+            team2 = sep.join([players[int(i)].strip() for i in matchup[2:]])            
+            items.append([idx + 1, court, team1, team2])
+            court += 1
+    return items
+
+
+def schedule_summary(players: List[str], sched: str) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    """Summarizes schedule by player
+    
+    Keys:
+        n_courts
+        n_players
+        opponent_dupcount
+        partner_dupcount
+        schedule - is a list of list of list of int
+        each inner list is a pairing
+        each outer list is a round
+        the far outer list is the schedule
+
+    """
+    # build up lists of tuples
+    # then can count at the end
+    # ultimately want to implement this as part of schedule optimizer
+    # this is a simple fix for the time being
+    sched = json.loads(sched)
+
+    partners = defaultdict(int)
+    opponents = defaultdict(int)
+
+    for rnd in sched:
+        for matchup in rnd:
+            team1 = tuple(sorted(matchup[0:2]))
+            team2 = tuple(sorted(matchup[2:]))
+            partners[team1] += 1
+            partners[team2] += 1
+            for opp in itertools.product(team1, team2):
+                opponents[tuple(sorted(opp))] += 1
+
+    dup_partners = {idx: 0 for idx in range(len(players))}
+    dup_opponents = {idx: 0 for idx in range(len(players))}
+    
+    for pairing, cnt in partners.items():
+        if cnt > 1:
+            player1, player2 = pairing
+            dup_partners[player1] += 1
+            dup_partners[player2] += 1
+            
+    for pairing, cnt in opponents.items():
+        if cnt > 1:
+            player1, player2 = pairing
+            dup_opponents[player1] += 1
+            dup_opponents[player2] += 1
+        
+    return ({players[k]: v for k, v in dup_partners.items()}, 
+            {players[k]: v for k, v in dup_opponents.items()})
+
+
+def test_summ():
+    """Tests the summary schedule"""
+    sched = json.dumps([[[1, 2, 3, 4], [5, 6, 7, 8]], [[2, 1, 4, 3], [6, 7, 5, 8]]])
+    r = schedule_summary(list(range(1, 9)), sched)
+    assert r['dup_opponents'] == {1: 2, 2: 2, 3: 2, 4: 2, 5: 1, 6: 1, 7: 1, 8: 1}, r['dup_opponents']
+    assert r['dup_partners'] == {1: 1, 2: 1, 3: 1, 4: 1, 5: 0, 6: 0, 7: 0, 8: 0}, r['dup_partners']
+    print('Tests passed!')
+
+
+if __name__ == '__main__':
+    test_summ()
